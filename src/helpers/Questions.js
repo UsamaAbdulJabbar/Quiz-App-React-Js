cb968b735188873776b5364343a0efb80f2a9271
USamaAbdulJAbbar <usamabest4u@gmail.com>
2022-05-13 16:10:30 +0500

    work Complete

---
+++ src/helpers/Questions.js
@@ -0,0 +1,34 @@
+export const Questions = [
+    {
+      prompt: "HTML stand for ?",
+      optionA: "Hosting Transfer Mail Link",
+      optionB: "Hyper Text Marking Language",
+      optionC: "Hyper Tranfer Markup Language",
+      optionD: "Hyper Text Markup Language",
+      asnwer: "optionD",
+    },
+    {
+      prompt: "Which of this is not a programming language?",
+      optionA: "C++",
+      optionB: "JavaScript",
+      optionC: "HTML",
+      optionD: "Python",
+      asnwer: "optionC",
+    },
+    {
+      prompt: "Which of this is not a javascript framework?",
+      optionA: "React",
+      optionB: "Angular",
+      optionC: "Vue",
+      optionD: "Java",
+      asnwer: "optionD",
+    },
+    {
+      prompt: "CSS stand for ?",
+      optionA: "Commission Short Serivice",
+      optionB: "Cascading Style Sheet",
+      optionC: "Component Styling Sheet",
+      optionD: "Creative Style Sheet",
+      asnwer: "optionB",
+    }
+  ];
